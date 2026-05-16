27cab640e7a2d4238ee95fbba274a8c9fe10a510
Gaurish Budhiraja <gaurishbudhiraja2005@gmail.com>
2026-05-16 14:23:05 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+        for(char c:s){
+            freq1[c - 'a']++;
+        }
+        for(char c:t){
+            freq2[c - 'a']++;
+        }
+        if(freq1 == freq2) return true;
+        return false;
+    }
+};
